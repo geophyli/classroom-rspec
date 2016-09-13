ad7ae22e42e542802fdaa389a0b0d088cb9b5a7e
geophy <ligeophy@gmail.com>
2016-09-13 13:41:32 +0800

    add FactoryGirl

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
+
 RSpec.describe CoursesController, type: :controller do
 
   describe "GET index" do
     it "assigns @courses and render template" do
-      course1 = Course.create(title: "foo", description: "bar")
-      course2 = Course.create(title: "bar", description: "foo")
 
+      course1 = FactoryGirl.create(:course)
+      course2 = FactoryGirl.create(:course)
       get :index
 
       expect(assigns[:courses]).to eq([course1,course2])
